020e7ff0cd4fea8997bdf766316bd6696fd140d7
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2026-05-24 14:58:51 +0530

    first commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main(){
+  int a=10;
+  int b=55;
+  int c=90;
+  int d=100;
+  int e=150;
+
+  print(a+b+c+d+e);
+  print(a*b*c*d*e);
+  print(a/b/c/d/e);
+  print(a-b-c-d-e);
+  print(a%b);
+}
